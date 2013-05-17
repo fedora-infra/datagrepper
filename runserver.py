96a5fb72acf2c15722f88b8269d5acc3c8762903
Ralph Bean <rbean@redhat.com>
2013-05-17 09:31:05 -0400

    Fix up the runserver script.

--- runserver.py
+++ runserver.py
@@ -3,5 +3,5 @@ import os
 if not os.environ.get("DATAGREPPER_CONFIG"):
     os.environ["DATAGREPPER_CONFIG"] = '../development.cfg'
 
-from datagrepper import app
+from datagrepper.app import app
 app.run()
