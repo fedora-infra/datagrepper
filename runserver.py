4c401bf250e7b11d8b1716827196456513daa618
Ian Weller <ian@ianweller.org>
2013-09-26 02:01:00 -0500

    Adjust base-directory scripts

--- runserver.py
+++ runserver.py
@@ -1,7 +1,2 @@
-import os
-
-if not os.environ.get("DATAGREPPER_CONFIG"):
-    os.environ["DATAGREPPER_CONFIG"] = '../development.cfg'
-
 from datagrepper.app import app
-app.run()
+app.run(debug=True)
