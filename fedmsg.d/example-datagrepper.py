668c7765607a81b7a32609da7d52fe6bced7ca67
Ralph Bean <rbean@redhat.com>
2014-04-28 12:16:17 -0400

    Use latest bootstrap and fix style on the docs pages.

--- fedmsg.d/example-datagrepper.py
+++ fedmsg.d/example-datagrepper.py
@@ -3,7 +3,7 @@ config = {
     # access to its DB.
     'datanommer.enabled': False,
     # This is generally not safe.. you probably want to use a real DB.
-    'datanommer.sqlalchemy.url': 'sqlite:////tmp/datanommer.db',
+    'datanommer.sqlalchemy.url': 'postgres://datanommer:bunbunbun@localhost/datanommer',
     # Enable this to enable the datagrepper job runner.
     'fedmsg.consumers.datagrepper-runner.enabled': True,
 }
