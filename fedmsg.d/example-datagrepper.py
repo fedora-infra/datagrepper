efd308d26d02bcc0e7332a540843e17c4d3915be
Ralph Bean <rbean@redhat.com>
2014-08-13 13:37:07 -0400

    Undo fedmsg.d silliness for #149.

--- fedmsg.d/example-datagrepper.py
+++ fedmsg.d/example-datagrepper.py
@@ -1,9 +1,9 @@
 config = {
     # We don't really want to *run* datanommer, but we do want
     # access to its DB.
-    'datanommer.enabled': True,
+    'datanommer.enabled': False,
     # This is generally not safe.. you probably want to use a real DB.
-    'datanommer.sqlalchemy.url': 'postgres://datanommer:bunbunbun@localhost/datanommer',
+    'datanommer.sqlalchemy.url': 'sqlite:////tmp/datanommer.db',
     # Enable this to enable the datagrepper job runner.
     'fedmsg.consumers.datagrepper-runner.enabled': True,
 }
