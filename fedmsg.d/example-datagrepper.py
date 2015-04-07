a7a128bc252e7e3437de83b1ffc2551f50ee82a8
Ralph Bean <rbean@redhat.com>
2015-04-07 10:47:47 -0400

    Add fedmenu.

--- fedmsg.d/example-datagrepper.py
+++ fedmsg.d/example-datagrepper.py
@@ -6,4 +6,12 @@ config = {
     'datanommer.sqlalchemy.url': 'sqlite:////tmp/datanommer.db',
     # Enable this to enable the datagrepper job runner.
     'fedmsg.consumers.datagrepper-runner.enabled': True,
+
+    # For production
+    #'fedmenu_url': 'https://apps.fedoraproject.org/fedmenu',
+    #'fedmenu_data_url': 'https://apps.fedoraproject.org/js/data.js',
+
+    # For development
+    #'fedmenu_url': 'http://threebean.org/fedmenu',
+    #'fedmenu_data_url': 'http://threebean.org/fedmenu/dev-data.js',
 }
