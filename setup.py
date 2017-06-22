5df0cb73fba7be34da1655b9fe6be4c14143ff20
Ralph Bean <rbean@redhat.com>
2017-06-21 20:15:22 -0400

    0.8.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def get_requires(filename="requirements.txt"):
 setup(
     name='datagrepper',
     description='A webapp to query fedmsg history',
-    version='0.7.1',
+    version='0.8.0',
     author='Ian Weller and Ralph Bean',
     author_email='ianweller@fedoraproject.org, ralph@fedoraproject.org',
     license='GPLv2+',
