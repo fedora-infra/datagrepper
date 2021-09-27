19fe79b1fcbd229cbb395ee524fe167a3afcc9f0
Aurélien Bompard <aurelien@bompard.org>
2021-09-27 12:17:53 +0200

    Move the extra template variables to the config
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- datagrepper/default_config.py
+++ datagrepper/default_config.py
@@ -6,6 +6,23 @@ CORS_MAX_AGE = "600"
 DEFAULT_QUERY_DELTA = 0
 DATANOMMER_SQLALCHEMY_URL = "postgresql://datanommer:datanommer@localhost/messages"
 DATAGREPPER_APPROXIMATE_COUNT = True
+BUS_INFO = {
+    "link": "https://fedora-messaging.readthedocs.io",
+    "shortname": "Fedora Messaging",
+    "longname": "Fedora Messaging bus",
+    "docs": "https://fedora-messaging.readthedocs.io",
+}
+THEME_CSS_URL = (
+    "https://apps.fedoraproject.org/global/fedora-bootstrap-1.0/"
+    "fedora-bootstrap.min.css"
+)
+
+# Example: "wss://hub.fedoraproject.org:9939"
+WEBSOCKET_URL = None
+# Only allow websockets connections to fedoraproject.org, for instance:
+# "connect-src https://*.fedoraproject.org wss://*.fedoraproject.org"
+CONTENT_SECURITY_POLICY = None
+
 HEALTHZ = {
     "live": "datagrepper.app.liveness",
     "ready": "datagrepper.app.readiness",
